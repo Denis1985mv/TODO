5fbf1c796951d6e3e925b5bb5e1d14aab7c2ff33
Dzianis <dzianis888mv@gmail.com>
2022-11-08 00:46:41 +0300

    Добавил обработчик событий

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,43 @@
-import { Person } from "./person";
+// import { Person } from "./person";
 
 
-const person = new Person;
-console.log(person.name);
+// const person = new Person;
+// console.log(person.name);
 // alert(person.name);
+//const el = document.getElementById("bottanTwo");
+//el.addEventListener("click", modifyText, false);
+
+const bottonTwo2 = document.querySelector('#bottanTwo');
+const bottanSection = document.querySelector('#bottanSection');
+console.log(bottonTwo2);
+bottonTwo2.addEventListener('click', function(){
+  console.log("TEST");
+
+  const valHTML = `
+  <div class="block">     
+  <p class="blockText">Name</p>
+  <p class="blockText">Homework</p>
+  </div>
+  `;
+
+  bottanSection.insertAdjacentHTML('afterend', valHTML);
+
+})
+
+const bottanOne = document.querySelector('#bottanOne');
+const bottanSectionOne = document.querySelector('#bottanSectionOne');
+
+bottanOne.addEventListener('click', function(){
+    console.log("TEST");
+  
+    const valHTMLOne = `
+                   <div class="bottonDawn bottonDawnPositionTop">
+                        <p>todo design</p>
+                    </div>
+    `;
+  
+    bottanSectionOne.insertAdjacentHTML('afterend', valHTMLOne);
+  
+  })
 
 
-let variable = '' +
-'<html>' +
-'    <head>' +
-'    <title>Your Title Here</title>' +
-'    </head>' +
-'' +
-'    <body bgcolor="FFFFFF">' +
-'        <center><img src="wtools.jpg" align="bottom"> </center>' +
-'        <hr>' +
-'        <a href="http://wtools.io">Best Online Web Tools</a>is a link to another nifty site' +
-'        <h1>This is a Header</h1>' +
-'        <h2>This is a Medium Header</h2>' +
-'        Send me mail at <a href="mailto:support@example.com">support@example.com</a>.' +
-'        <p> This is a new paragraph!</p>' +
-'        <p> <b>This is a new paragraph!</b></p>' +
-'        <br> <b><i>This is a new sentence without a paragraph break, in bold italics.</i></b>' +
-'        <hr>' +
-'    </body>' +
-'</html>' +
-'';
-
-document.writeln(variable);
